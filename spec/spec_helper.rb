b95b102b140a5555b095b882349ea428efd210d2
Tim Smith <tsmith84@gmail.com>
2015-10-30 11:08:42 -0700

    Resolve rspec deprecation warning
    "RSpec::Core::Configuration#treat_symbols_as_metadata_keys_with_true_values=
    is deprecated, it is now set to true as default and setting it to false
    has no effect."

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require 'wmi-lite'
 
 RSpec.configure do |config|
   config.include(RSpec::Matchers)
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.filter_run_excluding :windows_only => true if ! (RUBY_PLATFORM =~ /mswin|mingw32|windows/)
   config.run_all_when_everything_filtered = true
